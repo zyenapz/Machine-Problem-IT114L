8ebc249b066e4d0eb6eb44d7e2bc0e47492619b3
Karrez2426 <72584770+Karrez2426@users.noreply.github.com>
2021-02-15 13:32:14 +0800

    Improved code structure

--- Machine-Problem-IT114L/EditInfo.aspx.cs
+++ Machine-Problem-IT114L/EditInfo.aspx.cs
@@ -9,13 +9,6 @@ using System.Data.OleDb;
 using System.Data;
 using System.IO; //For image upload
 
-/*
-Dev Notes:
-    -Change file Path of ImgIUpload.SaveAs() in CheckChanges() depende sa device niyo
-     Double \\ dapat reserved ata yung single \
-    -If a text box is left blank, no changes will be made to that particular field
-*/
-
 namespace MP_Prototype
 {
     public partial class WebForm7 : System.Web.UI.Page
@@ -29,20 +22,29 @@ namespace MP_Prototype
         {
             string[] updateData = new string[7];
             updateData = CheckChanges();
+            bool valid = ValidatePass();
+            if (valid)
+            {
+                string connstr = "Provider=Microsoft.Jet.OleDB.4.0; Data Source=";
+                connstr += Server.MapPath("~/App_Data/db_TradeLedger.Mdb");
+                OleDbConnection conn = new OleDbConnection(connstr);
+                conn.Open();
 
-            string connstr = "Provider=Microsoft.Jet.OleDB.4.0; Data Source=";
-            connstr += Server.MapPath("~/App_Data/db_TradeLedger.Mdb");
-            OleDbConnection conn = new OleDbConnection(connstr);
-            conn.Open();
+                OleDbCommand cmd = new OleDbCommand("update tbl_Trader set Trader_Lastname = '" + updateData[2] + "', Trader_Firstname = '" + updateData[1] + "', Trader_Address = '" +
+                    updateData[3] + "', Trader_Pass = '" + updateData[6] + "', Trader_ContactNum = '" + updateData[4] + "', Trader_Email = '" + updateData[5] + "', Trader_Image = '" +
+                    updateData[0] + "' where Trader_ID = " + CurrentID.current, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
 
-            OleDbCommand cmd = new OleDbCommand("update tbl_Trader set Trader_Lastname = '"+updateData[2]+"', Trader_Firstname = '"+updateData[1]+"', Trader_Address = '"+
-                updateData[3]+"', Trader_Pass = '"+updateData[6]+"', Trader_ContactNum = '"+updateData[4]+"', Trader_Email = '"+updateData[5]+"', Trader_Image = '"+
-                updateData[0]+"' where Trader_ID = "+CurrentID.current, conn);
-            cmd.ExecuteNonQuery();
-            conn.Close();
-
-            MessageBox.Show("Information Updated Successfully!");
-            Response.Redirect("MyAccount.aspx");  
+                MessageBox.Show("Information Updated Successfully!");
+                Response.Redirect("MyAccount.aspx");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Current Password.");
+                txtCurrPass.Text = "";
+            }
+            
         }
 
         public string[] CheckChanges()
@@ -69,34 +71,11 @@ namespace MP_Prototype
                 imgFile = reader[8].ToString();
 
             first = txtFirst.Text;
-            if (first.Trim() == "")
-                first = reader[2].ToString();
-
             last = txtLast.Text;
-            if (last.Trim() == "")
-                last = reader[1].ToString();
-
             add = txtAdd.Text;
-            if (add.Trim() == "")
-                add = reader[3].ToString();
-
             num = txtNum.Text;
-            if (num.Trim() == "")
-                num = reader[5].ToString();
-
             email = txtEmail.Text;
-            if (email.Trim() == "")
-                email = reader[6].ToString();
-
             pass = txtNewPass.Text;
-            if (pass.Trim() == "")
-                pass = reader[4].ToString();
-            else
-            {
-                bool valid = ValidatePass();
-                if (!valid)
-                    pass = reader[4].ToString();
-            }
 
             conn.Close();
 
@@ -126,23 +105,8 @@ namespace MP_Prototype
             bool valid = true;
             if (txtCurrPass.Text != reader["Trader_Pass"].ToString())
             {
-                MessageBox.Show("Incorrect Current Password. No changes will be made to password.");
-                txtCurrPass.Text = "";
-                txtNewPass.Text = "";
-                txtConfirmNew.Text = "";
                 valid = false;
             }                 
-            else
-            {
-                if (txtNewPass.Text != txtConfirmNew.Text)
-                {
-                    MessageBox.Show("Password does not match. No changes will be made to password.");
-                    txtCurrPass.Text = "";
-                    txtNewPass.Text = "";
-                    txtConfirmNew.Text = "";
-                    valid = false;
-                }           
-            }
 
             conn.Close();
             return valid;
