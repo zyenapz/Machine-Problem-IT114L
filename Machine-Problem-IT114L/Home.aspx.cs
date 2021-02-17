a2e334844f046400fcd0e2835d9f3d833e007747
zyenapz <zyenapz@gmail.com>
2021-02-17 16:55:03 +0800

    updated home.aspx

--- Machine-Problem-IT114L/Home.aspx.cs
+++ Machine-Problem-IT114L/Home.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
 
 namespace MP_Prototype
 {
@@ -11,7 +13,38 @@ namespace MP_Prototype
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Load_ProductGrid();
+        }
 
+        private void Load_ProductGrid()
+        {
+            string connstr = "Provider=Microsoft.Jet.OleDB.4.0; Data Source=" + Server.MapPath("~/App_Data/db_TradeLedger.mdb");
+            using (OleDbConnection conn = new OleDbConnection(connstr))
+            {
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM tbl_Product";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                int rowCount = Convert.ToInt32(cmd.ExecuteScalar());
+
+                if (rowCount == 0) 
+                {
+                    LabelEmpty.Text = "Looks like there are no products yet...";
+                    // LabelEmpty.Visible = true;
+                }
+                else
+                {
+                    query = "SELECT TOP 10 product_name FROM tbl_Product";
+                    cmd = new OleDbCommand(query, conn);
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    GridViewProducts.DataSource = dt;
+                    GridViewProducts.DataBind();
+                    // GridViewProducts.HeaderRow.Cells[0].Text = "Product";
+                }
+
+            }
         }
     }
 }
